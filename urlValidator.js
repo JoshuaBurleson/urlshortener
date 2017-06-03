0e161d92fec2cace857f66f5c632c9313e8292a2
Joshua Burleson <Joshua Burleson>
2017-06-03 00:08:29 -0400

    Initial commit

---
+++ urlValidator.js
@@ -0,0 +1,9 @@
+var validator = function(str) {
+  var regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
+  if(!regex .test(str)) {
+    return false;
+  } else {
+    return true;
+  }
+}
+module.exports = validator;
